518ee10fac227b35d446b6e01bdba1f46f3b24c9
Adrian Pascu <me.adi.hf@gmail.com>
2022-05-08 14:38:33 +0300

    InteractOnColision is false when changing direction

--- Assets/Scripts/Interactable/AbstractInteractableHandler.cs
+++ Assets/Scripts/Interactable/AbstractInteractableHandler.cs
@@ -5,41 +5,57 @@ using UnityEngine;
 
 namespace Interactable
 {
-    public abstract class AbstractInteractableHandler:MonoBehaviour
+    public abstract class AbstractInteractableHandler : MonoBehaviour
 
     {
         // True if the collision should start the interaction
         private bool _interactOnCollision = false;
+
+        // WHen _interactOnCollision is set to true, remember where the player clicked
+        // If the player clicks again to change direction, disable the interaction  on collision
+        private Vector2 _characterDirectionWhenClicked;
         private Camera _mainCamera;
-       protected abstract void OnInteract();
 
-       public MainCharacterMovement mainCharacterMovement;
+        protected abstract void OnInteract();
 
-       protected void StopInteraction()
-       {
-           _interactOnCollision = false;
-       }
+        public MainCharacterMovement mainCharacterMovement;
 
-       void Start()
-       {
-           _mainCamera = Camera.main;
-       }
-       
+        protected void StopInteraction()
+        {
+            _interactOnCollision = false;
+        }
 
-       private void OnMouseDown()
-       {
-           _interactOnCollision = true;
-       }
+        void Start()
+        {
+            _mainCamera = Camera.main;
+        }
 
-       private void OnTriggerEnter2D(Collider2D other)
-       {
-           if (other.gameObject.layer == LayerMask.NameToLayer(Layers.MainCharacterLayer))
-           {
-               if(_interactOnCollision)
-               {
-                   OnInteract();
-               }
-           }
-       }
+
+        private void OnMouseDown()
+        {
+            _interactOnCollision = true;
+            _characterDirectionWhenClicked = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && mainCharacterMovement.CanMoveOnClick() &&
+                _characterDirectionWhenClicked != (Vector2) _mainCamera.ScreenToWorldPoint(Input.mousePosition))
+            {
+                // The user changed direction
+                _interactOnCollision = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer(Layers.MainCharacterLayer))
+            {
+                if (_interactOnCollision)
+                {
+                    OnInteract();
+                }
+            }
+        }
     }
 }
