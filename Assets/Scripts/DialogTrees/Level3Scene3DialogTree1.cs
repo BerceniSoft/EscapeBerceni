30ecd80d85dacdf5a61fdcf590ab6e637d1d14c5
Adrian Pascu <me.adi.hf@gmail.com>
2022-05-23 20:07:52 +0300

    Started working on final scene of lv 3

---
+++ Assets/Scripts/DialogTrees/Level3Scene3DialogTree1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Dialog;
+using DialogTrees;
+using UnityEngine;
+
+public class Level3Scene3DialogTree1 : AbstractDialogTree
+{
+    public override int OnDialogOptionPicked(int optionIndex, int dialogLineIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override DialogLineInfo GetDialogLine(int dialogLineIndex)
+    {
+        switch (dialogLineIndex)
+        {
+            case 0:
+            {
+                return new DialogLineInfo("Aici se odihn")
+            }
+        }
+    }
+}
