1f0f6d622d3dcdf6306f5ff3ddb0bd96a83d0fa1
adi_random <me.adi.hf@gmail.com>
2022-05-26 17:21:33 +0300

    Added letter inventory system

---
+++ Assets/Scripts/Inventory/ScritpableObjects/LetterScriptableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Letter")]
+public class LetterScriptableObject : ScriptableObject
+{
+    public Sprite icon;
+    public bool has;
+    
+}
