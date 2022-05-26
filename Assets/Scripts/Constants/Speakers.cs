c8e4a165da73a4cd915fc8b1af654c6d1771538d
adi_random <me.adi.hf@gmail.com>
2022-05-26 17:01:22 +0300

    Bug fixes

--- Assets/Scripts/Constants/Speakers.cs
+++ Assets/Scripts/Constants/Speakers.cs
@@ -9,7 +9,7 @@ namespace Constants
         public const string Eminescu = "Eminescu";
         public const string Megaphone = "Megafon";
         public const string Grave1 = "Nume Mormânt";
-        public const string EminescuGrave = "Mihai Eminescu (15.0.1.1850 - 15.06/1889)";
+        public const string EminescuGrave = "Mihai Eminescu (15.01.1850 - 15.06.1889)";
         public const string Preot = "Preot";
     }
 }
