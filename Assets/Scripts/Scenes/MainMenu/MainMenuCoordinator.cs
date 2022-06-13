b33a57f345367023dab4c3ea8c4fb1940bd7b3be
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2022-06-13 18:52:03 +0300

    Add main menu.

---
+++ Assets/Scripts/Scenes/MainMenu/MainMenuCoordinator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes.MainMenu
+{
+    public class MainMenuCoordinator : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Frateli");
+        }
+    }
+}
